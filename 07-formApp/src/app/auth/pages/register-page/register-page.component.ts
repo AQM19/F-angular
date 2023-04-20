7ca3e4496fcd98311bab5300f7c46ea762bac772
Aaron Quintanal Martín <a.quintanal@lksnext.com>
2023-04-20 16:52:55 +0200

    Actualizacion de Angular

---
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
+//import * as customValidators from 'src/app/shared/validators/validators';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styles: [
+  ]
+})
+export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+    ) { }
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+
+}
